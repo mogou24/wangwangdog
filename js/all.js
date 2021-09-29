3f7efc3cc7fcd7eacce9066297113821003635ef
mogou24 <86212920+mogou24@users.noreply.github.com>
2021-09-29 10:16:38 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,15 +1,3 @@
-$(document).ready(function () {
-    $("a").on("click", function () {
-      var hrefLink = $(this).attr("href");
-      console.log(hrefLink);
-      $("html,body").animate(
-        {
-          scrollTop: $(hrefLink).offset().top+100 //直接到相對位置
-        },
-        1200
-      );
-    });
-  });
 
   /* AOS */
   AOS.init();
@@ -31,12 +19,5 @@ $(document).ready(function () {
 // Bind as an event handler
 $(document).on('click', '[data-lightbox]', lity);
 
+//滑鼠換圖
 
-// /* 滾動移除class */
-//   $(window).scroll(function(){            //當視窗滾動時
-//         if ( $(this).scrollTop() > 5) {     //如果視窗向下滾動100px
-//           $('.circlepic').addClass('animate__fadeInRight');  //header 加上我們剛剛寫好的scroll css
-//         } else {
-//           $('.circlepic').addClass('animate__fadeInRight'); //如果沒有的話就刪除 scroll css
-//        }
-//     });
